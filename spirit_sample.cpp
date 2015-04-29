1a28f3b2c23cbd76373cc89123121f66898ba18b
Alexander Pivovarov <pivizz@gmail.com>
2015-04-29 08:35:12 -0700

    Spirit sample

---
+++ spirit_sample.cpp
@@ -0,0 +1,64 @@
+#include <boost/config/warning_disable.hpp>
+#include <boost/spirit/include/qi.hpp>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+namespace qi = boost::spirit::qi;
+namespace ascii = boost::spirit::ascii;
+
+template <typename Iterator>
+struct BrainfuckPPGrammar : qi::grammar<Iterator>
+{
+    BrainfuckPPGrammar() : BrainfuckPPGrammar::base_type(program)
+    {
+        using qi::eps;
+        using qi::lit;
+        using qi::_val;
+        using qi::_1;
+        using qi::_r1;
+        using ascii::char_;
+
+        program = eps         //  [_r1 << "Start parsing"]
+                  >>
+                  char_('B')  //  [_r1 << "Symbol B found"]
+        ;
+/*
+        start = eps             [_val = 0] >>
+            (
+                +lit('M')       [_val += 1000]
+                ||  hundreds    [_val += _1]
+                ||  tens        [_val += _1]
+                ||  ones        [_val += _1]
+            )
+        ;
+*/
+    }
+
+    qi::rule<Iterator> program;
+};
+
+int main()
+{
+    typedef BrainfuckPPGrammar<std::string::const_iterator> grammar;
+    grammar g; // Our grammar
+
+    vector<string> samples;
+    samples.push_back("B");
+    samples.push_back("BX");
+    samples.push_back("B X");
+    samples.push_back("B ");
+    samples.push_back("E");
+    for (string& storage : samples)
+    {
+      string::const_iterator iter = storage.begin();
+      string::const_iterator end = storage.end();
+
+      bool r = qi::parse(iter, end, g);
+      cout << "String '" << storage << "': " << r << " " << (iter == end) << endl;
+    }
+    return 0;
+}
+
